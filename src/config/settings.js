93dbcee409610c7115317191ef9f780448b6999a
Darkarotte <The127@users.noreply.github.com>
2025-05-05 22:01:40 +0200

    Add PostgreSQL integration with connection pooling (#4)
    ### Description
    
    This pull request introduces the following changes:
    
    - Adds a PostgreSQL connection pool with error handling to enhance
    database interactions.
    - Introduces a `POSTGRES_DB` environment variable to `docker-compose`
    for increased configurability.
    - Includes the `pg` dependency to support PostgreSQL integration.

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,9 +1,16 @@
 import 'dotenv/config';
 
 export const config = {
-  server: {
-    port: process.env.PORT || 5680,
-    host: process.env.HOST || '0.0.0.0'
-  },
-  logLevel: process.env.LOG_LEVEL || 'info'
+    server: {
+        port: process.env.PORT || 5680,
+        host: process.env.HOST || '0.0.0.0'
+    },
+    db: {
+        user: process.env.POSTGRES_USER || 'user',
+        password: process.env.POSTGRES_PASSWORD || 'password',
+        host: process.env.POSTGRES_HOST || 'localhost',
+        port: process.env.POSTGRES_PORT || 7891,
+        database: process.env.POSTGRES_DB || 'postgres'
+    },
+    logLevel: process.env.LOG_LEVEL || 'info'
 };
