cf42ed3ba53f2dbdbb05402e2fcf39b73f4afb1e
karo <karolin.kostial@gmail.com>
2025-05-06 20:11:55 +0200

    feat(container): add service registration and resolution functionality with tests

--- src/container/__tests__/container.js
+++ src/container/__tests__/container.js
@@ -1,10 +1,29 @@
 import {Container} from "../index.js";
 import { jest } from '@jest/globals';
 
+class TestInterface {
+    doSomething() {}
+}
+
+class TestImplementation {
+    doSomething() {}
+}
+
+
 test('registering non-class interface throws error', () => {
     const container = new Container()
 
     expect(() => {
         container.register('notAClass', () => {})
     }).toThrow('Interface must be a class')
+})
+
+test('can register and resolve implementation', () => {
+    const container = new Container()
+    const implementation = new TestImplementation()
+
+    container.register(TestInterface, implementation)
+    const resolved = container.resolve(TestInterface)
+
+    expect(resolved).toBe(implementation)
 })
--- src/container/index.js
+++ src/container/index.js
@@ -1,8 +1,15 @@
 // src/container/container.js
 export class Container {
+  #services = new Map()
+
   register(interfaceType, implementation) {
     if (!interfaceType?.prototype) {
       throw new Error('Interface must be a class')
     }
+    this.#services.set(interfaceType, implementation)
+  }
+
+  resolve(interfaceType) {
+    return this.#services.get(interfaceType)
   }
 }
