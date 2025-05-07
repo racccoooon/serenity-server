db117dbdb5a881fe6ecbf537f85cb1b12b3451bb
karo <karolin.kostial@gmail.com>
2025-05-07 18:09:42 +0200

    feat: add basic user register api endpoint (dummy)

---
+++ src/routes/users.js
@@ -0,0 +1,35 @@
+import { z } from 'zod';
+import { extendZodWithOpenApi } from 'zod-openapi';
+
+extendZodWithOpenApi(z);
+
+const userRegisterSchema = z.object({
+    username: z.string().openapi({
+        description: 'Username of the user that is registering.',
+        example: 'bean27'
+    }),
+    email: z.string(),
+    authenticationMethods: z.array(z.discriminatedUnion("type", [
+            z.object({
+                type: z.literal('password'),
+                details: z.object({
+                    password: z.string(),
+                })
+            })
+        ])
+    ).nonempty()
+});
+
+export async function registerUser(fastify) {
+    fastify.post('/api/v1/users/register', {
+        schema: {
+            body: userRegisterSchema,
+        },
+    }, async (request, reply) => {
+
+        reply.code(200).send({
+          error: 'Internal Server Error Cheese',
+          message: 'Error registering user'
+        });
+    });
+}
