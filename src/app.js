e50ef0d67b7144bc1811542626ee2f9be9f33ce6
karo <karolin.kostial@gmail.com>
2025-05-07 19:13:59 +0200

    chore: move mediator setup to app.js

--- src/app.js
+++ src/app.js
@@ -3,12 +3,13 @@ import {logger} from './utils/logger.js';
 import {config} from './config/settings.js';
 import {routes} from './routes/index.js';
 import {runMigrations} from "./db/index.js";
-import {initializeMediator} from './mediator/instance.js'
 import {serializerCompiler, validatorCompiler} from "fastify-zod-openapi";
-import {Container} from "container";
+import {Container} from "./container/index.js";
 import {UserDomainService} from "./services/userDomainService.js";
 import {UserRepository} from "./repositories/userRepository.js";
 import {UserAuthRepository} from "./repositories/userAuthRepository.js";
+import {Mediator} from "./mediator/index.js";
+import {handleRegisterUser, RegisterUserCommand} from "./commands/users/registerUser.js";
 
 const fastify = Fastify({logger: false});
 
@@ -20,19 +21,22 @@ fastify.setSerializerCompiler(serializerCompiler);
 
 export const container = new Container();
 
-container.registerSingleton(UserDomainService, (c) => new UserDomainService({
+container.registerTransient(UserDomainService, (c) => new UserDomainService({
     userRepository: c.resolve(UserRepository),
     userAuthRepository: c.resolve(UserAuthRepository),
 }));
 
-container.registerSingleton(UserRepository, () => new UserRepository());
-container.registerSingleton(UserAuthRepository, () => new UserAuthRepository());
+container.registerTransient(UserRepository, () => new UserRepository());
+container.registerTransient(UserAuthRepository, () => new UserAuthRepository());
+
+export const mediator = new Mediator();
+
+mediator.register(RegisterUserCommand, handleRegisterUser);
 
 // Start the server
 const start = async () => {
     try {
         await runMigrations();
-        initializeMediator()
 
         await fastify.listen(config.server);
         logger.info(`Server is running on http://localhost:${config.server.port}`);
