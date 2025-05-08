5a77b336b650d30724962211849f44d96961292e
karo <karolin.kostial@gmail.com>
2025-05-08 22:47:36 +0200

    feat: throw error when user is not found

--- src/commands/__tests__/passwordLogin.js
+++ src/commands/__tests__/passwordLogin.js
@@ -1,6 +1,23 @@
-import {PasswordLoginHandler} from "../auth/passwordLogin.js";
+import {AuthorizationError, PasswordLoginCommand, PasswordLoginHandler} from "../auth/passwordLogin.js";
+import { jest } from '@jest/globals';
 
 test('Login user without command', async () => {
     const handler = new PasswordLoginHandler();
     expect(handler.handle()).rejects.toThrow('Command must be provided');
-})
+});
+
+test('Login unknown user', async () => {
+    // arrange
+    const userService = {
+        findUser: jest.fn(() => null),
+    }
+
+    const command = {
+        username: "UnknownBean",
+    };
+
+    const handler = new PasswordLoginHandler(userService);
+
+    // act
+    expect(handler.handle(command)).rejects.toThrow(AuthorizationError)
+});
--- src/commands/auth/passwordLogin.js
+++ src/commands/auth/passwordLogin.js
@@ -1,8 +1,15 @@
 export class PasswordLoginCommand {
-
+    constructor(userSelector) {
+        this.userSelector = userSelector;
+    }
 }
 
+export class AuthorizationError extends Error{}
+
 export class PasswordLoginHandler {
+    constructor(userDomainService) {
+        this.userDomainService = userDomainService;
+    }
 
     /**
      * @param command PasswordLoginCommand
@@ -10,5 +17,9 @@ export class PasswordLoginHandler {
     handle = async(command) => {
         if (!command) throw new Error('Command must be provided');
 
+        const user = this.userDomainService.findUser(command.userSelector);
+        if(!user){
+            throw new AuthorizationError();
+        }
     }
 }
