2f8a541b7d2cf7709034932e21c704af1177e734
karo <karolin.kostial@gmail.com>
2025-05-09 12:42:25 +0200

    feat: implement user querying

--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -1,4 +1,6 @@
 import {pool} from "../db/index.js";
+import {UserId, UserName} from "../domain/user.js";
+import {logger} from "../utils/logger.js";
 
 export class CreateUserModel {
     constructor(id, username, email) {
@@ -8,7 +10,13 @@ export class CreateUserModel {
     }
 }
 
-export class UserModel {}
+export class UserModel {
+    constructor(id, username, email) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+    }
+}
 
 export class UserRepository {
     async add(param) {
@@ -19,6 +27,28 @@ export class UserRepository {
     }
 
     async find(selector) {
+        let sqlb = new Sqlb(
+            `select id, username, email from users where`
+        );
 
+        switch (true){
+            case selector.value instanceof UserId:
+                sqlb.add('id = $id', {id: selector.value.value});
+                break;
+            case selector.value instanceof UserName:
+                sqlb.add('username = $username', {username: selector.value.value});
+        }
+
+        const {sql, params} = sql.build();
+        logger.debug("executing sql: ", sql);
+        const result = await pool.query(sql, params);
+
+        const users = result.rows.map(row => new UserModel(row.id, row.username, row.email));
+
+        if (users.length === 1) {
+            return users[0];
+        }
+
+        return null;
     }
 }
