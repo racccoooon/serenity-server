dc4679350299d5a3432452b93c9a0c4e2cd21e88
karo <karolin.kostial@gmail.com>
2025-05-12 16:25:31 +0200

    feat: implement authentication groud work for remote instances

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,14 @@
 import {Mediator} from "../mediator/index.js";
 import {GetPublicUserProfileQuery} from "../queries/users/getPublicUserProfile.js";
 import {status} from "http-status";
+import {authenticateEntity} from "./_httpAuth.js";
+import {AuthError} from "../errors/authError.js";
 
 export function getPublicUserProfile(fastify) {
     fastify.get('/api/v1/users/:id',
         async (request, reply) => {
+            await authenticateEntity(request);
+
             const response = await request.scope.resolve(Mediator)
                 .send(new GetPublicUserProfileQuery(
                     request.params.id,
