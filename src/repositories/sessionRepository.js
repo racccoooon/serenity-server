c85497694009e0377e9bd83e1f67be4f59fdaf87
Gwendolyn <me@gwendolyn.dev>
2025-05-15 16:47:27 +0200

    shrimplify sessionRepository

--- src/repositories/sessionRepository.js
+++ src/repositories/sessionRepository.js
@@ -1,5 +1,6 @@
 import {SqlRepository} from "./_sqlRepository.js";
 import {Sqlb} from "./_sqlb.js";
+import {sql} from "./_shrimple.js";
 
 export class SessionFilter {
     whereId(id) {
@@ -10,24 +11,16 @@ export class SessionFilter {
 
 export class SessionRepository extends SqlRepository {
     get insertIntoSql() {
-        return 'insert into sessions (id, user_id, salt, hashed_secret, valid_until)';
-    }
-
-    get insertRowSql() {
-        return `($id, $userId, $salt, $hashedSecret, now() + interval '7 days')`;
+        return sql`insert into sessions (id, user_id, salt, hashed_secret, valid_until)`;
     }
 
     mapToTable(model) {
-        return {
-            id: model.id,
-            userId: model.userId,
-            salt: model.salt,
-            hashedSecret: model.hashedSecret,
-        };
+        return sql`(${model.id}, ${model.userId}, ${model.salt}, ${model.hashedSecret}, now() + interval '7 days')`;
     }
 
     buildSelectFromFilter(filter) {
-        return this.sqlWithWhereClause(new Sqlb('select * from sessions'), filter);
+        return this.sqlWithWhereClause(sql`select *
+                                           from sessions`, filter);
     }
 
     mapFromTable(row) {
@@ -40,24 +33,28 @@ export class SessionRepository extends SqlRepository {
     }
 
     buildDeleteFromFilter(filter) {
-        return this.sqlWithWhereClause(new Sqlb('delete from sessions'), filter);
+        return this.sqlWithWhereClause(sql`delete
+                                           from sessions`, filter);
     }
 
-    sqlWithWhereClause(sqlb, filter) {
-        sqlb.add('where true');
+    sqlWithWhereClause(shrimple, filter) {
+        const clauses = [];
 
         if (filter.filterId !== undefined) {
-            sqlb.add('and id = $id', {id: filter.filterId});
+            clauses.push(sql`id
+            =
+            ${filter.filterId}`);
         }
 
-        return sqlb;
+        shrimple.appendMany(clauses, 'and', 'where');
+
+        return shrimple;
     }
 
     async updateUsageAndValidUntil(filter) {
-        const sqlb = new Sqlb('update sessions');
-        sqlb.add(`set last_used = now(), valid_until = now() + interval '7 days'`)
-        this.sqlWithWhereClause(sqlb, filter);
-        const result = await this.execute(sqlb);
+        const shrimple = sql`update sessions set last_used = now(), valid_until = now() + interval '7 days'`;
+        this.sqlWithWhereClause(shrimple, filter);
+        const result = await this.execute(shrimple);
         return result.rowCount;
     }
 }
